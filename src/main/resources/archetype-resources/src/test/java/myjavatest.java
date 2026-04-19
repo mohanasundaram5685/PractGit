47102324e4d18abe7ead32ff9d5e6e20475d48e8
Yuvasri <mohanasundaram5685@gmail.com>
2026-04-19 15:42:01 +0530

    hello commit

--- src/main/resources/archetype-resources/src/test/java/myjavatest.java
+++ src/main/resources/archetype-resources/src/test/java/myjavatest.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
     System.out.println("hello world");
     System.out.println("hello mohana");
     System.out.println("hello sundaram sundaram");
+    System.out.println("hello team");
 }
         }
