a9428fc98b623878861fd5e27be3287d029b5b41
mohanasundaram5685 <mohanasundaram5685@gmail.com>
2026-04-19 15:35:36 +0530

    Update greeting message for 'sundaram'

--- src/main/resources/archetype-resources/src/test/java/myjavatest.java
+++ src/main/resources/archetype-resources/src/test/java/myjavatest.java
@@ -4,6 +4,6 @@ class myjavatest {
 public static void main(String[] args) {
     System.out.println("hello world");
     System.out.println("hello mohana");
-    System.out.println("hello sundaram");
+    System.out.println("hello sundaram sundaram");
 }
         }
