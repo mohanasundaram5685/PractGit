8b71d0a87229d3ddb9368fabd35a05f6460857c4
Yuvasri <mohanasundaram5685@gmail.com>
2026-04-19 15:34:33 +0530

    message2

--- src/main/resources/archetype-resources/src/test/java/myjavatest.java
+++ src/main/resources/archetype-resources/src/test/java/myjavatest.java
@@ -4,5 +4,6 @@ class myjavatest {
 public static void main(String[] args) {
     System.out.println("hello world");
     System.out.println("hello mohana");
+    System.out.println("hello sundaram");
 }
         }
